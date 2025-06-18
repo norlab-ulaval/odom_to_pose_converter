db9224372fa1e607197f5ae66c2c28ceb506e094
norlab-warthog <norlabwarthog@gmail.com>
2025-06-18 09:52:18 -0400

    weekly deployment  by [OG]

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
 
-class OdomToPoseNode : public rclcpp::Node
+class OdomToPathNode : public rclcpp::Node
 {
 public:
-    OdomToPoseNode() :
-        Node("odom_to_pose_node")
+    OdomToPathNode() :
+        Node("odom_to_path_node")
     {
+        this->declare_parameter<int>("buffer_size", 100);
+        this->get_parameter("buffer_size", bufferSize);
+
         odomSubscription = this->create_subscription<nav_msgs::msg::Odometry>("odom_in", 1000,
-                                                                              std::bind(&OdomToPoseNode::subscriptionCallback, this,
+                                                                              std::bind(&OdomToPathNode::subscriptionCallback, this,
                                                                                         std::placeholders::_1));
-        posePublisher = this->create_publisher<geometry_msgs::msg::PoseStamped>("pose_out", 1000);
+        pathPublisher = this->create_publisher<nav_msgs::msg::Path>("path_out", 1000);
     }
 
 private:
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odomSubscription;
-    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr posePublisher;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pathPublisher;
+    nav_msgs::msg::Path path;
+    int bufferSize;
 
     void subscriptionCallback(const nav_msgs::msg::Odometry& odometryMsg)
     {
@@ -24,15 +30,21 @@ private:
         poseStamped.header.frame_id = odometryMsg.header.frame_id;
         poseStamped.header.stamp = odometryMsg.header.stamp;
         poseStamped.pose = odometryMsg.pose.pose;
-        posePublisher->publish(poseStamped);
+        path.poses.push_back(poseStamped);
+	path.header.frame_id = odometryMsg.header.frame_id;
+	path.header.stamp = odometryMsg.header.stamp;
+        if(path.poses.size() > bufferSize)
+        {
+            path.poses.erase(path.poses.begin());
+        }
+        pathPublisher->publish(path);
     }
-
 };
 
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<OdomToPoseNode>());
+    rclcpp::spin(std::make_shared<OdomToPathNode>());
     rclcpp::shutdown();
     return 0;
 }
